e161566a389d3081bb1d6a0e54caa0c8c6f91fc1
Ali Hassan Liaquat <liaqqahl@gmail.com>
2023-11-14 01:40:32 +0000

    Complete project

--- javascript.js
+++ javascript.js
@@ -7,21 +7,52 @@ const container = document.querySelector('.container');
 
 function createGrid(val){
 
-    for (i=0; i<16; i++){
+    newSize = 640 / val; 
+
+    for (i=0; i<val; i++){
         const row = document.createElement('div');
         container.appendChild(row);
 
-        for(j=0; j<16; j++){
+        for(j=0; j<val; j++){
             const column = document.createElement('div');
             column.classList.add('column');
+            column.style.height = newSize + "px";
+            column.style.width = newSize + "px";
             row.appendChild(column);
         }
     }
-
-    val = val
 }
 
-createGrid(16);
+createGrid(16);      
+
+//creating button 
+
+const button = document.createElement('button');
+button.textContent = 'Choose Size!';
+button.classList.add('btn');
+document.body.insertBefore(button, container);
+
+
+//Event method for button
+
+button.addEventListener('click', () => {
+    const userInput = prompt('How many squares per size would you like?');
+    if (userInput <= 100) {
+        container.innerHTML = ' ';
+        createGrid(userInput);
+        const cellList = document.querySelectorAll('.column');
+
+        cellList.forEach(column => {
+        column.addEventListener('mouseover', () => {
+            column.classList.add('hovered');
+    })
+})
+    }
+
+    else{
+        console.log("Nay, sorry! Either too many or invalid input");
+    }
+})
 
 const cellList = document.querySelectorAll('.column');
 
